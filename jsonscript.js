37df3be0d5d1dfce0622b857211c80f80d1374ba
casperchristensendk <casp838@stud.kea.dk>
2022-02-24 09:47:35 +0100

    .

--- jsonscript.js
+++ jsonscript.js
@@ -1,33 +1,3 @@
-// Lav en variabel, der refererer til ".toggle-btn"
-const btn = document.querySelector(".toggle-btn");
-// Lav en variabel, der refererer til "nav"
-const nav = document.querySelector("nav");
-
-// Lav en function, der hedder toggleMenu()
-function toggleMenu() {
-  nav.classList.toggle("shown");
-
-  // 1. Toggle en klasse på nav vha. classList.toggle
-  // 2. Toggle en klasse, der hedder "shown"
-
-  // 1. Lav en variabel, der hedder menuShown
-  // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
-
-  const menuShown = nav.classList.contains("Shown");
-  // 1. Lav en if/else sætning => if (...) {...} else {...}
-  // 2. Spørg om menu i if-sætningen => if (menu)
-  if (menuShown == true) {
-    btn.textContent = "X";
-    // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
-  } else {
-    btn.textContent = "☰";
-    // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
-  }
-}
-
-/* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
-btn.addEventListener("click", toggleMenu);
-
 const header = document.querySelector("header h1");
 const myHeaders = {
   "x-apikey": "620e1ecc34fd6215658586f9",
